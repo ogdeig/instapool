8de0d99f34494dd56d2e6ae7b251e2fa01ec619e
Free Crypto Miner <72282547+ogdeig@users.noreply.github.com>
2021-02-01 09:31:38 -0600

    Add files via upload

---
+++ InstaPool Cpu Miner/start_mining_cpuchain.bat
@@ -0,0 +1,2 @@
+cpuminer.exe -a cpupower -o stratum+tcp://instapool.xyz:3333 -u CM8wHs2M6ECxZJQy19iG6B6qceR9Ut6Dy5
+pause
---
+++ InstaPool Cpu Miner/start_mining_sugarchain.bat
@@ -0,0 +1,2 @@
+cpuminer.exe -a yespowersugar -o stratum+tcp://instapool.xyz:3032 -u sugar1qnd3mamq6e7wsrml40ve95h3y3l9u9sq7gj0fc0
+pause
